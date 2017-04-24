4987488b94e2e1adfd8c2418ca860ba14c8f2a10
Mike <weldonconsulting@gmail.com>
2017-04-23 18:43:57 -0700

    send pending messages at end of broadcastPatch. probably naive approach

--- src/Room.ts
+++ src/Room.ts
@@ -32,7 +32,9 @@ export abstract class Room<T> extends EventEmitter {
   private _simulationInterval: NodeJS.Timer;
   private _patchInterval: number;
 
-  constructor ( options: any = {} ) {
+  private _delayedMessage: { client: Client, data: any }[] = [];
+
+  constructor(options: any = {}) {
     super()
 
     this.roomId = options.roomId;
@@ -41,66 +43,70 @@ export abstract class Room<T> extends EventEmitter {
     this.options = options;
 
     // Default patch rate is 20fps (50ms)
-    this.setPatchRate( 1000 / 20 );
+    this.setPatchRate(1000 / 20);
   }
 
-  abstract onMessage (client: Client, data: any): void;
-  abstract onJoin (client: Client, options?: any): void;
-  abstract onLeave (client: Client): void;
-  abstract onDispose (): void;
+  abstract onMessage(client: Client, data: any): void;
+  abstract onJoin(client: Client, options?: any): void;
+  abstract onLeave(client: Client): void;
+  abstract onDispose(): void;
 
-  public requestJoin (options: any): boolean {
+  public requestJoin(options: any): boolean {
     return true;
   }
 
-  public setSimulationInterval ( callback: Function, delay: number = 1000 / 60 ): void {
+  public setSimulationInterval(callback: Function, delay: number = 1000 / 60): void {
     // clear previous interval in case called setSimulationInterval more than once
-    if ( this._simulationInterval ) clearInterval( this._simulationInterval );
+    if (this._simulationInterval) clearInterval(this._simulationInterval);
 
-    this._simulationInterval = setInterval( () => {
+    this._simulationInterval = setInterval(() => {
       this.clock.tick();
       callback();
-    }, delay );
+    }, delay);
   }
 
-  public setPatchRate ( milliseconds: number ): void {
+  public setPatchRate(milliseconds: number): void {
     // clear previous interval in case called setPatchRate more than once
-    if ( this._patchInterval ) clearInterval(this._patchInterval);
+    if (this._patchInterval) clearInterval(this._patchInterval);
 
-    this._patchInterval = setInterval( this.broadcastPatch.bind(this), milliseconds );
+    this._patchInterval = setInterval(this.broadcastPatch.bind(this), milliseconds);
   }
 
-  public useTimeline ( maxSnapshots: number = 10 ): void {
-    this.timeline = createTimeline( maxSnapshots );
+  public useTimeline(maxSnapshots: number = 10): void {
+    this.timeline = createTimeline(maxSnapshots);
   }
 
-  public setState (newState) {
+  public setState(newState) {
     this.clock.start();
 
     // ensure state is populated for `sendState()` method.
-    this._previousState = toJSON( newState );
-    this._previousStateEncoded = msgpack.encode( this._previousState );
+    this._previousState = toJSON(newState);
+    this._previousStateEncoded = msgpack.encode(this._previousState);
 
     this.state = newState;
 
-    if ( this.timeline ) {
-      this.timeline.takeSnapshot( this.state );
+    if (this.timeline) {
+      this.timeline.takeSnapshot(this.state);
     }
   }
 
-  public lock (): void {
+  public lock(): void {
     this.emit('lock');
   }
 
-  public unlock (): void {
+  public unlock(): void {
     this.emit('unlock');
   }
 
-  public send (client: Client, data: any): void {
-    client.send( msgpack.encode( [Protocol.ROOM_DATA, this.roomId, data] ), { binary: true }, logError.bind(this) );
+  public send(client: Client, data: any, delay?: boolean): void {
+    if (!delay) {
+      return client.send(msgpack.encode([Protocol.ROOM_DATA, this.roomId, data]), { binary: true }, logError.bind(this));
+    } else {
+      this._delayedMessage.push({ client: client, data: data });
+    }
   }
 
-  public broadcast (data: any): boolean {
+  public broadcast(data: any): boolean {
     // no data given, try to broadcast patched state
     if (!data) {
       throw new Error("Room#broadcast: 'data' is required to broadcast.");
@@ -113,64 +119,69 @@ export abstract class Room<T> extends EventEmitter {
 
     var numClients = this.clients.length;
     while (numClients--) {
-      this.clients[ numClients ].send(data, { binary: true }, logError.bind(this) );
+      this.clients[numClients].send(data, { binary: true }, logError.bind(this));
     }
 
     return true;
   }
 
-  public disconnect (): void {
+  public disconnect(): void {
     var i = this.clients.length;
     while (i--) {
       this._onLeave(this.clients[i]);
     }
   }
 
-  protected sendState (client: Client): void {
-    client.send( msgpack.encode( [
+  protected sendState(client: Client): void {
+    client.send(msgpack.encode([
       Protocol.ROOM_STATE,
       this.roomId,
       this._previousState,
       this.clock.currentTime,
       this.clock.elapsedTime,
-    ] ), {
-      binary: true
-    }, logError.bind(this) );
+    ]), {
+        binary: true
+      }, logError.bind(this));
   }
 
-  private broadcastPatch (): boolean {
-    if ( !this._previousState ) {
-      throw new Error( 'trying to broadcast null state. you should call #setState on constructor or during user connection.' );
+  private broadcastPatch() {
+    if (!this._previousState) {
+      throw new Error('trying to broadcast null state. you should call #setState on constructor or during user connection.');
     }
 
-    let currentState = toJSON( this.state );
-    let currentStateEncoded = msgpack.encode( currentState );
+    let currentState = toJSON(this.state);
+    let currentStateEncoded = msgpack.encode(currentState);
 
     // skip if state has not changed.
-    if ( currentStateEncoded.equals( this._previousStateEncoded ) ) {
-      return false;
+    if (!currentStateEncoded.equals(this._previousStateEncoded)) {
+      let patches = fossilDelta.create(this._previousStateEncoded, currentStateEncoded);
+
+      // take a snapshot of the current state
+      if (this.timeline) {
+        this.timeline.takeSnapshot(this.state, this.clock.elapsedTime);
+      }
+
+      this._previousState = currentState;
+      this._previousStateEncoded = currentStateEncoded;
+
+      // broadcast patches (diff state) to all clients,
+      // even if nothing has changed in order to calculate PING on client-side
+      this.broadcast(msgpack.encode([Protocol.ROOM_STATE_PATCH, this.roomId, patches]));
     }
 
-    let patches = fossilDelta.create( this._previousStateEncoded, currentStateEncoded );
-
-    // take a snapshot of the current state
-    if (this.timeline) {
-      this.timeline.takeSnapshot( this.state, this.clock.elapsedTime );
+    // send any pending delayed messages
+    for (let i = this._delayedMessage.length - 1; i >= 0; i--) {
+      this.send(this._delayedMessage[i].client, this._delayedMessage[i].data);
     }
-
-    this._previousState = currentState;
-    this._previousStateEncoded = currentStateEncoded;
-
-    // broadcast patches (diff state) to all clients,
-    // even if nothing has changed in order to calculate PING on client-side
-    return this.broadcast( msgpack.encode([ Protocol.ROOM_STATE_PATCH, this.roomId, patches ]) );
+    // clear the pending message list
+    this._delayedMessage.length = 0;
   }
 
-  private _onJoin (client: Client, options?: any): void {
-    this.clients.push( client );
+  private _onJoin(client: Client, options?: any): void {
+    this.clients.push(client);
 
     // confirm room id that matches the room name requested to join
-    client.send( msgpack.encode( [Protocol.JOIN_ROOM, this.roomId, this.roomName] ), { binary: true }, logError.bind(this) );
+    client.send(msgpack.encode([Protocol.JOIN_ROOM, this.roomId, this.roomName]), { binary: true }, logError.bind(this));
 
     // send current state when new client joins the room
     if (this.state) {
@@ -182,7 +193,7 @@ export abstract class Room<T> extends EventEmitter {
     }
   }
 
-  private _onLeave (client: Client, isDisconnect: boolean = false): void {
+  private _onLeave(client: Client, isDisconnect: boolean = false): void {
     // remove client from client list
     spliceOne(this.clients, this.clients.indexOf(client));
 
@@ -191,14 +202,14 @@ export abstract class Room<T> extends EventEmitter {
     this.emit('leave', client, isDisconnect);
 
     if (!isDisconnect) {
-      client.send( msgpack.encode( [Protocol.LEAVE_ROOM, this.roomId] ), { binary: true }, logError.bind(this) );
+      client.send(msgpack.encode([Protocol.LEAVE_ROOM, this.roomId]), { binary: true }, logError.bind(this));
     }
 
     // custom cleanup method & clear intervals
-    if ( this.clients.length == 0 ) {
-      if ( this.onDispose ) this.onDispose();
-      if ( this._patchInterval ) clearInterval( this._patchInterval );
-      if ( this._simulationInterval ) clearInterval( this._simulationInterval );
+    if (this.clients.length == 0) {
+      if (this.onDispose) this.onDispose();
+      if (this._patchInterval) clearInterval(this._patchInterval);
+      if (this._simulationInterval) clearInterval(this._simulationInterval);
 
       this.emit('dispose');
     }
--- test/RoomTest.ts
+++ test/RoomTest.ts
@@ -9,25 +9,25 @@ import {
   DummyRoomWithState
 } from "./utils/mock";
 
-describe('Room', function() {
+describe('Room', function () {
 
-  describe('#constructor', function() {
+  describe('#constructor', function () {
 
-    it('should instantiate with valid options', function() {
-      var room = new DummyRoom({ });
+    it('should instantiate with valid options', function () {
+      var room = new DummyRoom({});
       assert.ok(room instanceof DummyRoom);
     });
 
-    it('should instantiate with timeline attribute', function() {
-      var room = new DummyRoomWithTimeline({ });
+    it('should instantiate with timeline attribute', function () {
+      var room = new DummyRoomWithTimeline({});
       assert.equal(0, room.timeline.history.length);
     });
 
   });
 
-  describe('#onJoin/#onLeave', function() {
-    it('should receive onJoin/onLeave messages', function() {
-      var room = new DummyRoom({ });
+  describe('#onJoin/#onLeave', function () {
+    it('should receive onJoin/onLeave messages', function () {
+      var room = new DummyRoom({});
       var client = createDummyClient();
       var message = null;
 
@@ -43,8 +43,8 @@ describe('Room', function() {
       assert.equal(message[0], Protocol.LEAVE_ROOM);
     });
 
-    it('should receive JOIN_ROOM and ROOM_DATA messages onJoin', function() {
-      var room = new DummyRoomWithState({ });
+    it('should receive JOIN_ROOM and ROOM_DATA messages onJoin', function () {
+      var room = new DummyRoomWithState({});
       var client = createDummyClient();
       var message = null;
 
@@ -59,15 +59,16 @@ describe('Room', function() {
       assert.equal(message[0], Protocol.ROOM_STATE);
     });
 
-    it('should cleanup/dispose when all clients disconnect', function(done) {
-      var room = new DummyRoom({ });
+    it('should cleanup/dispose when all clients disconnect', function (done) {
+      var room = new DummyRoom({});
       var client = createDummyClient();
 
       (<any>room)._onJoin(client);
-      assert.equal(typeof((<any>room)._patchInterval._repeat), "number");
+      assert.equal(typeof ((<any>room)._patchInterval._repeat), "number");
 
-      room.on('dispose', function() {;
-        assert.equal(typeof((<any>room)._patchInterval._repeat), "object");
+      room.on('dispose', function () {
+        ;
+        assert.equal(typeof ((<any>room)._patchInterval._repeat), "object");
         done();
       });
 
@@ -75,17 +76,17 @@ describe('Room', function() {
     });
   });
 
-  describe('patch interval', function() {
-    it('should set default "patch" interval', function() {
-      var room = new DummyRoom({ });
-      assert.equal("object", typeof((<any>room)._patchInterval));
+  describe('patch interval', function () {
+    it('should set default "patch" interval', function () {
+      var room = new DummyRoom({});
+      assert.equal("object", typeof ((<any>room)._patchInterval));
       assert.equal(1000 / 20, (<any>room)._patchInterval._idleTimeout, "default patch rate should be 20");
     });
   });
 
-  describe('#sendState', function() {
-    it('should send state when it is set up', function() {
-      let room = new DummyRoom({ });
+  describe('#sendState', function () {
+    it('should send state when it is set up', function () {
+      let room = new DummyRoom({});
       let client = createDummyClient();
       (<any>room)._onJoin(client, {});
 
@@ -95,16 +96,16 @@ describe('Room', function() {
       (<any>room).sendState(client);
 
 
-      var message = msgpack.decode( client.messages[1] );
+      var message = msgpack.decode(client.messages[1]);
 
       assert.equal(message[0], Protocol.ROOM_STATE);
       assert.deepEqual(message[2], { success: true });
     });
   });
 
-  describe('#broadcastPatch', function() {
-    it('should fail to broadcast patch without state', function() {
-      let room = new DummyRoom({ });
+  describe('#broadcastPatch', function () {
+    it('should fail to broadcast patch without state', function () {
+      let room = new DummyRoom({});
 
       // connect 2 dummy clients into room
       let client1 = createDummyClient();
@@ -117,8 +118,8 @@ describe('Room', function() {
       assert.throws(() => { (<any>room).broadcastPatch(); });
     });
 
-    it('should broadcast patch having state', function() {
-      let room = new DummyRoom({ });
+    it('should broadcast patch having state', function () {
+      let room = new DummyRoom({});
 
       // connect 2 dummy clients into room
       let client1 = createDummyClient();
@@ -128,21 +129,19 @@ describe('Room', function() {
       (<any>room)._onJoin(client2, {});
 
       // set state
-      room.setState({one: 1});
-      assert.deepEqual({one: 1}, room.state);
+      room.setState({ one: 1 });
+      assert.deepEqual({ one: 1 }, room.state);
 
-      // clean state. no patches available!
-      assert.equal(false, (<any>room).broadcastPatch());
+      (<any>room).broadcastPatch();
 
       // change the state to make patch available
       room.state.one = 111;
 
-      // voila!
-      assert.equal(true, (<any>room).broadcastPatch());
+      (<any>room).broadcastPatch();
     });
 
-    it('shouldn\'t broadcast clean state (no patches)', function() {
-      var room = new DummyRoom({ });
+    it('shouldn\'t broadcast clean state (no patches)', function () {
+      var room = new DummyRoom({});
       room.setState({ one: 1 });
 
       // create 2 dummy connections with the room
@@ -152,17 +151,17 @@ describe('Room', function() {
       var client2 = createDummyClient();
       (<any>room)._onJoin(client2, {});
 
-      assert.deepEqual({one: 1}, room.state);
+      assert.deepEqual({ one: 1 }, room.state);
 
       // clean state. no patches available!
-      assert.equal(false, (<any>room).broadcastPatch());
+      (<any>room).broadcastPatch();
 
       // change the state to make patch available
       room.state.two = 2;
-      assert.deepEqual({one: 1, two: 2}, room.state);
+      assert.deepEqual({ one: 1, two: 2 }, room.state);
 
-      // voila!
-      assert.equal(true, (<any>room).broadcastPatch());
+
+      (<any>room).broadcastPatch();
 
       assert.equal(client.messages.length, 3);
       assert.equal(client2.messages.length, 3);
@@ -180,7 +179,7 @@ describe('Room', function() {
       assert.equal(message[0], Protocol.ROOM_STATE_PATCH);
       assert.deepEqual(message[2].length, 22);
 
-      assert.deepEqual(message[2], [ 66, 10, 66, 58, 130, 163, 111, 110, 101, 1, 163, 116, 119, 111, 2, 49, 86, 53, 49, 74, 89, 59 ]);
+      assert.deepEqual(message[2], [66, 10, 66, 58, 130, 163, 111, 110, 101, 1, 163, 116, 119, 111, 2, 49, 86, 53, 49, 74, 89, 59]);
     });
   });
 
